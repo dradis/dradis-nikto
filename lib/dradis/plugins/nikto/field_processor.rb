7413b9211210ad71ec7dd7e6d02bfbe6400384d1
Jose A. Rilla <jose@rilla.es>
2014-12-16 21:56:46 +0100

    Use TemplateService for item rendering

--- lib/dradis/plugins/nikto/field_processor.rb
+++ lib/dradis/plugins/nikto/field_processor.rb
@@ -2,6 +2,19 @@ module Dradis
   module Plugins
     module Nikto
       class FieldProcessor < Dradis::Plugins::Upload::FieldProcessor
+
+        def post_initialize(args={})
+          @nikto_object = ::Nikto::Item.new(data)
+        end
+
+        def value(args={})
+          field = args[:field]
+          # fields in the template are of the form <foo>.<field>, where <foo>
+          # is common across all fields for a given template (and meaningless).
+          _, name = field.split('.')
+          @nikto_object.try(name) || 'n/a'
+        end
+
       end
     end
   end
