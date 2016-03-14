5a74d3c5ea48845e31cfcccb3b11601999343466
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:50:58 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/nikto.rb
+++ lib/dradis/plugins/nikto.rb
@@ -9,19 +9,3 @@ require 'dradis/plugins/nikto/engine'
 require 'dradis/plugins/nikto/field_processor'
 require 'dradis/plugins/nikto/importer'
 require 'dradis/plugins/nikto/version'
-
-# This is required while we transition the Upload Manager to use
-# Dradis::Plugins only
-module Dradis
-  module Plugins
-    module Nikto
-      module Meta
-        NAME = "Nikto XML upload plugin"
-        EXPECTS = "Nikto results XML. Use the -o switch with a file name ending in .xml"
-        module VERSION
-          include Dradis::Plugins::Nikto::VERSION
-        end
-      end
-    end
-  end
-end
